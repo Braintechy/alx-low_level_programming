cb2ae85412e788a3c6a46476e2a40ab9fa2b4607
Braintechy <yusuffmuhammed140@gmail.com>
2023-10-10 06:31:06 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print _putchar with a new line.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
