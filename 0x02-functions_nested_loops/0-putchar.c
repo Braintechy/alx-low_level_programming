4111969d05a4c0672926016f1e859a50e2ca9b0a
Braintechy <yusuffmuhammed140@gmail.com>
2023-10-10 08:56:54 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * _putchar - write out _putchar with new line
  *
  * Return: Always 0 (success)
  */
 int main(void)
-
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 
 /**
- * _putcahr - write the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: on successs 1.
- * on error, -1 is returned, and errno is set appropriately.
+ * Return: on success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
