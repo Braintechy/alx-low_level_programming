ecc5736239e752895e73677bac04451ae18f44b4
Braintechy <yusuffmuhammed140@gmail.com>
2023-10-10 10:41:18 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,12 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: The character to be printed
- * Return: 1 for character, or 0 for anything else
+ * Return: 1 for character or 0 for anything else
  */
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
-	
 	return (1);
 	}
 	return (0);
