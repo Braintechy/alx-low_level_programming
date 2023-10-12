2e467a9259d71151fda60037c08c770023d14758
Braintechy <yusuffmuhammed140@gmail.com>
2023-10-12 16:00:54 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints 10 times the number since 0 up to 14
- * Return: 10 times of the number since 0 up to 14
+ * more_numbers - Print 10 times the numbers since 0 up to 14
+ * Return: 10 times of the numbers since 0 up to 14
  */
 void more_numbers(void)
 {
@@ -10,7 +10,7 @@ void more_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-	for (y = 0; y < 14; y++)
+	for (y = 0; y <= 14; y++)
 	{
 	if (y > 9)
 	{
