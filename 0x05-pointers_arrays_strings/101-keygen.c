967a164d824f8216311d7f7f34d06c52c3de0745
Braintechy <yusuffmuhammed140@gmail.com>
2023-10-16 16:04:27 -0700

    Task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -34,6 +34,19 @@ int main(void)
 		for (index = 0; password[index]; index++)
 
 		{
+			if (password[index] >= (33 + diff_half1))
+
+			{
+
+				password[index] -= diff_half1;
+
+				break;
+			}
+		}
+
+		for (index = 0; password[index]; index++)
+
+		{
 			if (password[index] >= (33 + diff_half2))
 
 			{
