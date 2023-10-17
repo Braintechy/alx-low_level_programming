ca94d3f20aa0dd0215d4be48974c22ddbac24bdd
Braintechy <yusuffmuhammed140@gmail.com>
2023-10-16 17:30:34 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,8 +7,11 @@
  * @n: Number of elements of the array to be printed
  * Return: Void
  */
+
 void print_array(int *a, int n)
+
 {
+
 	int j;
 
 	for (j = 0; j < n; j++)
@@ -16,7 +19,7 @@ void print_array(int *a, int n)
 	printf("%d", a[j]);
 	if (j != (n - 1))
 	{
-	printf(",");
+	printf(", ");
 	}
 	}
 	printf("\n");
