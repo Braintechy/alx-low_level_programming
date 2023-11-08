a69b1f9ea331845c4cb5b30a2889eb84ef139e2a
Braintechy <yusuffmuhammed140@gmail.com>
2023-11-08 08:53:42 -0800

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,9 +14,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array != NULL && action != NULL && size > 0)
 	{
-	while (i > size)
+	while (i < size)
 	{
-	action (array[i]);
+	action(array[i]);
 	i++;
 	}
 	}
